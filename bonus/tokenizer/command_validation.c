733e98f9be9544740a0ca01dae9c8e0551e2ba19
Eduardo Moraes(edu-bm7) <bm7.eduardo@gmail.com>
2023-08-03 20:32:15 -0300

    WIP: finished builtin and command validation, moving on to handling when is not a command or builtin

--- bonus/tokenizer/command_validation.c
+++ bonus/tokenizer/command_validation.c
@@ -1,5 +1,9 @@
 #include "minishell_bonus.h"
 
+t_bool	free_vars_and_return_true(char **dir, char *cmd);
+t_bool	check_path_var(char *token);
+void	free_dir(char **dir);
+
 t_bool	is_builtin(char *token, t_grammar *grammar)
 {
 	size_t	i;
@@ -16,7 +20,7 @@ t_bool	is_builtin(char *token, t_grammar *grammar)
 
 t_bool	is_command(char *token)
 {
-	struct stat stats;
+	struct	stat stats;
 
 	if (stat(token, &stats) == -1) {
 		ft_dprintf(2,"Failed to get file status");
@@ -33,5 +37,49 @@ t_bool	is_command(char *token)
 		exit(NOTEXEC);
 	}
 	if (access(token, F_OK) == -1)
+		return (check_path_var(token));
+	return (true);
+}
 
+t_bool	check_path_var(char *token)
+{
+	char	*path;
+	char	**dir;
+	char	*tmp;
+	char	*cmd;
+	int		i;
+
+	i = 0;
+	path = getenv("PATH");
+	dir = ft_split(path, ':');
+	while(dir[i])
+	{
+		tmp = ft_strjoin(dir[i], "/");
+		cmd = ft_strjoin(tmp, token);
+		free(tmp);
+		if (access(cmd, X_OK) == 0)
+			free_vars_and_return_true(dir, cmd);
+		free(cmd);
+		cmd = NULL;
+		i++;
+	}
+	free_dir(dir);
+	return (false);
+}
+
+void	free_dir(char **dir)
+{
+	int	i;
+
+	i = 0;
+	while(dir[i])
+		free(dir[i++]);
+	free(dir);
+}
+
+t_bool	free_vars_and_return_true(char **dir, char *cmd)
+{
+	free(cmd);
+	free_dir(dir);
+	return (true);
 }
