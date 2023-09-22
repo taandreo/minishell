ebafe4da79aebdeac4278a0e7edec69071e2e5fa
Eduardo Moraes(edu-bm7) <bm7.eduardo@gmail.com>
2023-09-22 14:47:50 -0300

    fix: fixed parse_string logic to recursive call it

--- bonus/parser/parse_arguments_bonus.c
+++ bonus/parser/parse_arguments_bonus.c
@@ -1,14 +1,10 @@
 #include "minishell_bonus.h"
 
-t_string	*get_next_string(t_token_list *tokens);
-
 t_arguments	*parse_arguments(t_token_list *tokens)
 {
 	t_arguments	*args;
 
 	args = NULL;
-	if (current_token_type(tokens) == TOKEN_END)
-		return (NULL);
 	if (current_token_type(tokens) == TOKEN_SPACE)
 		advance_token(tokens);
 	if (current_token_type(tokens) == TOKEN_STRING
@@ -29,33 +25,27 @@ t_string	*parse_string(t_token_list *tokens)
 {
 	t_string	*string;
 
-	string = malloc(sizeof(t_string));
-	if (!string)
-	{
-		free_string(string);
-		return (return_mem_alloc_error());
-	}
-	string->type = current_token_type(tokens);
-	string->value = ft_strdup(current_token(tokens).value);
-	if (!string->value)
-	{
-		free_string(string);
-		return (return_mem_alloc_error());
-	}
-	advance_token(tokens);
-	string->next = get_next_string(tokens);
-	return (string);
-}
-
-t_string	*get_next_string(t_token_list *tokens)
-{
-	t_string	*string;
-
 	string = NULL;
 	if (current_token_type(tokens) != TOKEN_SPACE
 			&& !(current_token_type(tokens) >= TOKEN_AND
 			&& current_token_type(tokens) <= TOKEN_ERROR)
 			&& current_token_type(tokens) != TOKEN_END)
-		string = parse_string(tokens);
+	{
+		string = malloc(sizeof(t_string));
+		if (!string)
+		{
+			free_string(string);
+			return (return_mem_alloc_error());
+		}
+		string->type = current_token_type(tokens);
+		string->value = ft_strdup(current_token(tokens).value);
+		if (!string->value)
+		{
+			free_string(string);
+			return (return_mem_alloc_error());
+		}
+		advance_token(tokens);
+		string->next = parse_string(tokens);
+	}
 	return (string);
 }
