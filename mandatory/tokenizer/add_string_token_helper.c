a6e97556fc71c7349f954631febd04bb97c1bbbc
Eduardo Moraes(edu-bm7) <bm7.eduardo@gmail.com>
2023-10-26 21:54:16 -0300

    fix: fixed empty string behaviour, now it shows command not found.

--- mandatory/tokenizer/add_string_token_helper.c
+++ mandatory/tokenizer/add_string_token_helper.c
@@ -25,7 +25,7 @@ int	add_builtin_or_command(char *string, t_token_list **tokens,
 	{
 		if (is_builtin(string))
 			exit_status = add_token(tokens, get_builtin_token(string), string);
-		else if (flags->inside_quotes && flags->quote_type == '\"')
+		else if (flags->inside_quotes || flags->quote_type == '\"')
 			exit_status = add_token(tokens, TOKEN_COMMAND_NAME_QUOTES, string);
 		else
 		{
